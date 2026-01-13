a1e392b7a6ab3b433c701ae529193b07448b4629
jahaa032 <jfhannestad@gmail.com>
2026-01-13 14:59:53 +0100

    Fixed button to title screen

--- rays/GDScripts/win_screen.gd
+++ rays/GDScripts/win_screen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://tscn/title_screen.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn") 
 
 
 func _on_quit_game_pressed() -> void:
