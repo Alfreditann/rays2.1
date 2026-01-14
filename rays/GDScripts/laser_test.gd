809fe7709d2042231ff8d81f3d8464367999427b
jahaa032 <jfhannestad@gmail.com>
2026-01-14 20:25:39 +0100

    Last push for today, Removed other animations from laser_test, still an animated sprite.

--- rays/GDScripts/laser_test.gd
+++ rays/GDScripts/laser_test.gd
@@ -6,7 +6,7 @@ var last_hit_time := 0.0
 var hit_delay := 0.2 # prevents multiple triggers per frame
 var rect_shape1 = 0
 @onready var anim = $AnimatedSprite2D
-func _ready():
+func _ready():	
 	linear_velocity = -global_transform.x * speed
 	$laser_test.area_entered.connect(_on_laser_test_area_entered)
 
