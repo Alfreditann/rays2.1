325d9c1ccc38f16327c8516bf790c7df4e409a1d
jahaa032 <jfhannestad@gmail.com>
2026-01-14 13:55:19 +0100

    Fixed Main Menu connecting to Level 1

--- rays/GDScripts/main_menu.gd
+++ rays/GDScripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_game_pressed() -> void: 
-	get_tree().change_scene_to_file("res://Scenes/pause_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
 
 func _on_settings_pressed() -> void:
 	pass
