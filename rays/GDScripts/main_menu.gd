ef4b9ac1361fd0337c3896cd6556fb48aef0bdb6
Dagrate123 <sigia002@osloskolen.no>
2026-01-15 14:05:26 +0100

    menu+settings

--- rays/GDScripts/main_menu.gd
+++ rays/GDScripts/main_menu.gd
@@ -5,7 +5,7 @@ func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
 
 func _on_settings_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
 #Gjør at når knappen blir trykket så lukker prosjektet seg.
 func _on_quit_game_pressed() -> void:
