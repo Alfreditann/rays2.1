9a956cbd48328d521fbabae8b2ca11e36424c729
Alfreditann <alfred.ulsberg@gmail.com>
2026-01-15 14:27:38 +0100

    leveldesing+bugs

--- rays/GDScripts/monster.gd
+++ rays/GDScripts/monster.gd
@@ -8,8 +8,3 @@ func _on_monster_area_entered(area: Area2D) -> void:
 		queue_free()	
 		#2 Change scene to win screen
 		get_tree().change_scene_to_file("res://Scenes/win_screen.tscn")
-
-
-
-func _on_monster_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
