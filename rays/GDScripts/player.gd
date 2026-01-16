733da43c62ca831216a0c290f619d539cfab497c
jahaa032 <jfhannestad@gmail.com>
2026-01-16 08:58:34 +0100

    Added a line to player so that it updates the raycast2d and added collision to level 3 and 1

--- rays/GDScripts/player.gd
+++ rays/GDScripts/player.gd
@@ -34,14 +34,22 @@ func _physics_process(_delta: float) -> void:
 func read_input():
 	var dir := Vector2.ZERO
 	# Tile-based movement input + collision check
-	if Input.is_action_pressed("move_up") and !$up.is_colliding():
-		dir = Vector2.UP
-	elif Input.is_action_pressed("move_down") and !$down.is_colliding():
-		dir = Vector2.DOWN
-	elif Input.is_action_pressed("move_left") and !$left.is_colliding():
-		dir = Vector2.LEFT
-	elif Input.is_action_pressed("move_right") and !$right.is_colliding():
-		dir = Vector2.RIGHT
+	if Input.is_action_pressed("move_up"):
+		$up.force_raycast_update()
+		if !$up.is_colliding():
+			dir = Vector2.UP
+	elif Input.is_action_pressed("move_down"):
+		$down.force_raycast_update()
+		if !$down.is_colliding():
+			dir = Vector2.DOWN
+	elif Input.is_action_pressed("move_left"):
+		$left.force_raycast_update()
+		if !$left.is_colliding():
+			dir = Vector2.LEFT
+	elif Input.is_action_pressed("move_right"):
+		$right.force_raycast_update()
+		if !$right.is_colliding():
+			dir = Vector2.RIGHT
 	
 	# Update last_dir when pressing movement keys
 	if dir != Vector2.ZERO:
