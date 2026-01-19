00b32c2a6781771165d0622cd59207af6ea2a67e
jahaa032 <jfhannestad@gmail.com>
2026-01-19 11:10:26 +0100

    Pushable mirror 94% working

--- rays/GDScripts/pushable_mirror.gd
+++ rays/GDScripts/pushable_mirror.gd
@@ -30,7 +30,7 @@ func _physics_process(delta: float) -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	lock_rotation = true
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
