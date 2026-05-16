5ce256da5dafa898929aaf6e9f95778e58767476
willbill0808 <williamlrosewell08@gmail.com>
2026-05-16 15:13:28 +0200

    grunt: laser polish
    Fjernet start_motion() siden den ikke gjorde noe.
    
    Endret også størelsen til laseren til å være kortere, sånn at den ser litt bedre etter den treffer.
    
    La til at laseren sjekker hvilket speil den traff sist, og bare godtar treffet om det ikke er samme, dette fikser dobbel hits. la også til _apply_bounce med call_defered, den kjører etter en frame er ferdig og passer på at spill motoren ikke krangler med koden.
    
    Nå skal det se ut som laseren faktisk treffer speilene og spretter av

--- rays/GDScripts/laser_test.gd
+++ rays/GDScripts/laser_test.gd
@@ -6,6 +6,7 @@ var last_hit_time := 0.0
 var hit_delay := 0.2 # prevents multiple triggers per frame
 var rect_shape1 = 0
 var dir := Vector2.ZERO
+var last_hit_mirror: Node = null  # ADD THIS
 @onready var anim = $AnimatedSprite2D
 
 func _ready():
@@ -26,22 +27,28 @@ func _ready():
 		linear_velocity = dir.normalized() * speed
 		rotation = dir.angle()
 
-func start_motion() -> void:
-	# Compute velocity from the current rotation
-	linear_velocity = Vector2.RIGHT.rotated(rotation).normalized() * speed
-	sleeping = false
-	# Debug:
-
 func _on_laser_test_area_entered(area: Area2D) -> void:
 	if area.name == "speil_hitbox":
+		
 		var mirror = area.get_parent()
+		
+		if mirror == last_hit_mirror: 
+			return
+		last_hit_mirror = mirror  
+		
 		var going = Decide_Vel(linear_velocity, mirror)
 		if going != null:
-			linear_velocity = going.normalized() * speed
+			dir = going
+			call_deferred("_apply_bounce", going)  # setter alle verdier etter at spill-motoren er ferdig med matten sin
+			
 	if area.name == "hurtbox":
 		queue_free()
 	if area.name == "monster":
 		queue_free()
+
+func _apply_bounce(going: Vector2) -> void:
+	linear_velocity = going.normalized() * speed
+	rotation = going.angle()
 
 func Decide_Vel(Current_Vel, mirror):
 	var mirror_dir = mirror.mirror_dir
