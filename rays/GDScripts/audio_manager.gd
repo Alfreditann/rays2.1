c3625961e991d48c5c6169da9e94b42101d51c9d
jahaa032 <jfhannestad@gmail.com>
2026-05-31 15:22:39 +0200

    Musikk for forskjellige scener

--- rays/GDScripts/audio_manager.gd
+++ rays/GDScripts/audio_manager.gd
@@ -1,4 +1,23 @@
 extends Node
 
+@export var menu_music: AudioStream
+@export var gameplay_music: AudioStream
+
+@onready var player = $MusicPlayer
+
+func _play_menu_music():
+	if player.stream == menu_music and player.playing:
+		return
+		
+	player.stream = menu_music
+	player.play()
+	
+func _play_gameplay_music():
+	if player.stream == gameplay_music and player.playing:
+		return
+		
+	player.stream = gameplay_music
+	player.play()
+
 func _ready() -> void:
 	pass
